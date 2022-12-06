8d67d31b6e35211d61d7943fa7b5dc8defca1238
JorshuaEs <estradajorshua16@gmail.com>
2022-12-06 12:34:26 -0600

    clase04

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import {valida} from "./validaciones.js";
+import {validar} from "./validaciones.js";
 
 const inputs = document.querySelectorAll("input");//seleccionar todos los input
 
 inputs.forEach( input =>{
     input.addEventListener('blur', (input) =>{
-        valida(input.target);
+        validar(input.target);
     })
 })//agregar el addevenlistener de blur cuando salga de foco y llamar la funcion de valida
--- js/validaciones.js
+++ js/validaciones.js
@@ -3,15 +3,62 @@ export function validar(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input);
     }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = " ";
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio"
+    },
+
+    email: {
+        valueMissing: "El campo correo no puede estar vacio",
+        typeMismatch: "El correo no es valido",
+    },
+
+    password: {
+        valueMissing: "El campo contraseña no puede estar vacio",
+        patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+    },
+
+    nacimiento: {
+        valueMissing: "El campo nacimiento no puede estar vacio",
+        customError: "Debes tener almenos 18 años de edad."
+    },
+
 }
 
 const validadores = {
     nacimiento: input => validarNacimiento(input)//se arma un objeto con los diferentes tipos de input que se tienen
 }
 
-inputNacimiento.addEventListener("blur", (evento) => {
-    validarNacimiento(evento.target);
-})
+function mostrarMensajeDeError(tipoDeInput, input){
+    let mensaje = " ";
+    tipoDeErrores.forEach(error =>{
+        if(input.validity[error]){
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    })
+    return mensaje
+}
 
 function validarNacimiento(input){
     const fechaCliente = new Date(input.value);
@@ -32,4 +79,3 @@ function mayorDeEdad(fecha){
     );
     return diferenciaFechas <= fechaActual;
 }
-//creacion de RegExr y acomodar los input y adevenlistener
